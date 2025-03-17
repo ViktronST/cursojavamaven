e9f727728c08bbb8081d8053e04419129534692f
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-03-17 20:49:15 +0100

    ConexionBaseDatos 2.0

--- src/main/java/es/cursojava/bbdd/ConexionBaseDatos.java
+++ src/main/java/es/cursojava/bbdd/ConexionBaseDatos.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import oracle.jdbc.pool.OracleDataSource;
+
 public class ConexionBaseDatos {
     public static void main(String[] args) {
-        
-        conectaOracle();
+
+        conectaOracle2();
     }
 
     private static void conectaOracle() {
@@ -17,9 +19,13 @@ public class ConexionBaseDatos {
 		String password = "password";
 
 		
+
 		try  {
-			Class.forName("oracle.jdbc.driver.OracleDriver");    // Esto es para librarias mas antiguas.
-			Connection connection = DriverManager.getConnection(url_oracle);
+			// Class.forName("oracle.jdbc.driver.OracleDriver");    // Esto es para librarias mas antiguas.
+			
+            Connection connection = DriverManager.getConnection(url_oracle);
+            //Connection connection = DriverManager.getConnection(url_oracle2, username, password);
+
 			if (connection!=null) {
 				System.out.println("Conexion establecida");
 			}
@@ -27,33 +33,41 @@ public class ConexionBaseDatos {
 		} catch (SQLException e) {  // Excepcion que viene de Connection.
 			System.err.println("Ha habido un error " + e.getMessage());
             // e.printStackTrace();
-		} catch (ClassNotFoundException e) {    // Excepcion que viene de Class.forName().
-			e.printStackTrace();
-		}
+		} // catch (ClassNotFoundException e) {    // Excepcion que viene de Class.forName().
+		// 	e.printStackTrace();
+		// }
 
 	}
 	
-	
-	// private void conectaOracle2() {
-	// 	String url_oracle = "jdbc:oracle:thin:curso/password@localhost:1521:XE";
-	// 	String url_oracle2 = "jdbc:oracle:thin:@localhost:1521:XE";
-	// 	String username = "curso";
-	// 	String password = "password";
+	// Versión más actualizada.
+	private static void conectaOracle2() {
+		String url_oracle = "jdbc:oracle:thin:lordvik/password@10.2.1.10:1522:XE";
+		String url_oracle2 = "jdbc:oracle:thin:@10.2.1.10:1522:XE";
+		String username = "lordvik";
+		String password = "password";
 
-	// 	try {
-	// 		OracleDataSource ods = new OracleDataSource();
-	// 		ods.setURL(url_oracle);
+        Connection connection = null;
+		try {
+			OracleDataSource ods = new OracleDataSource();
+			ods.setURL(url_oracle);
 			
-	// 		Connection connection = ods.getConnection();
-	// 		if (connection!=null) {
-	// 			System.out.println("Conexion establecida");
-	// 		}
+			connection = ods.getConnection();
+			if (connection!=null) {
+				System.out.println("Conexion establecida");
+			}
 			
-	// 	} catch (SQLException e) {
-	// 		// TODO Auto-generated catch block
-	// 		e.printStackTrace();
-	// 	}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
-	// }
+
+
+	}
 }
 
