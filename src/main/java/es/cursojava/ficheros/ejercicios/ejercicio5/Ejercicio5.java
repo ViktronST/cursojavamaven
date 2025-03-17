54ad1c9a71bdd2ddf8d581c8eebda861641148ce
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-03-17 18:23:39 +0100

    Ejercicio5 2.0

--- src/main/java/es/cursojava/ficheros/ejercicios/ejercicio5/Ejercicio5.java
+++ src/main/java/es/cursojava/ficheros/ejercicios/ejercicio5/Ejercicio5.java
@@ -2,42 +2,128 @@ package es.cursojava.ficheros.ejercicios.ejercicio5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-        cargarEventosFichero("D:\\ficheros\\eventos.txt");
+        Map<String, List<Evento>> mapa = cargarEventosFichero("D:\\ficheros\\eventos.txt", 1);
+        pintaMapa(mapa);
+        
+        //eventosPorCiudad(mapa);
+
+        eventosGratuitoPorCiudad(mapa);
     }
 
-    private static void cargarEventosFichero(String eventsFile) {
-        
+    private static void eventosGratuitoPorCiudad (Map<String, List<Evento>> mapa) {
+        System.out.println("========== Eventos gratuitos por Ciudad ==========");
+        Set<String> ciudades = mapa.keySet();
+        for (String ciudad : ciudades) {
+            List<Evento> eventosCiudad = mapa.get(ciudad);
+            for (Evento evento : eventosCiudad) {
+                if (evento.isGratuido()) {
+                    System.out.println(ciudad + " " + evento.getNombre());
+                }
+            }
+        }
+        System.out.println("Terminar");
+    }
+
+    // private static void eventosPorCiudad (Map<String, List<Evento>> mapa) {
+    //     Set<String> ciudades = mapa.keySet();
+    //     for (String ciudad : ciudades) {
+    //         Path ruta = Paths.get("./ciudades/" + ciudad + ".txt");
+
+    //         List<Evento> eventosCiudad = mapa.get(ciudad);
+    //         System.out.println("Ciudad: " + ciudad + " con " + eventosCiudad.size());
+    //         StringBuilder sb = new StringBuilder();
+    //         for (Evento evento : eventosCiudad) {
+    //             sb.append(evento.getNombre() + "\n");
+    //         }
+    //         Files.write(ruta, sb.toString().getBytes());
+    //     }
+    // }
+
+    private static void pintaMapa (Map<String, List<Evento>> mapa) {
+        Set<String> ciudades = mapa.keySet();
+        for (String ciudad : ciudades) {
+            List<Evento> eventosCiudad = mapa.get(ciudad);
+            System.out.println("Ciudad: " + ciudad + " con " + eventosCiudad.size());
+            for (Evento evento : eventosCiudad) {
+                System.out.println("\t" + evento.getNombre());
+            }
+        }
+    }
+
+    private static Map<String, List<Evento>> cargarEventosFichero(String eventsFile, int numLineas) {
+        Map<String, List<Evento>> mapaEventos = new HashMap<>();
+
         try (Scanner scan = new Scanner(new File(eventsFile))) {
             scan.useDelimiter("\n");
-            scan.nextLine();
+
+            for (int i = 0; i < numLineas; i++) {
+                scan.nextLine();
+            }
+
             while (scan.hasNext()) {
                 String linea = scan.nextLine();
+
                 String[] datos = linea.split(",");
+                // StringTokenizer st = new StringTokenizer(linea, ",");    // Es otra manera de trocear.
 
-                int id = Integer.parseInt(datos[0]);
+                int id = Integer.parseInt(datos[0]);    // Convertimos a entero el primer String que le pasamos.
+
                 String nombre = datos[1];
-                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-                Date fecha = format.parse(datos[2]);
-                String lugar = datos[3];
-                String descripcion = datos[4];
-                int duracion = Integer.parseInt(datos[5]);
-                double precio = Integer.parseInt(datos[6]);
-                String organizador = datos[7];
-                // boolean gratuito = Boolean.parseBoolean(datos[8]);
-                boolean gratuito = datos[8].equals("true")?true:false;
 
-                Evento evento = new Evento(id, linea, null, linea, eventsFile, 0, 0, linea, false);
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+                Date fecha = null;
+                try {
+                    fecha = format.parse(datos[2]);
+                } catch (ParseException e) {    // Obligados a capturar la excepcion por si los datos no vienen de manera correcta.
+                    e.printStackTrace();
+                }
+
+                String ciudad = datos[3];
+
+                String descripcion = datos[4];
+
+                int duracion = Integer.parseInt(datos[5]);
+
+                double precio = Double.parseDouble(datos[6]);
+
+                String organizador = datos[7];
+
+                boolean gratuito = Boolean.parseBoolean(datos[8]);
+                //boolean gratuito = datos[8].contains("true")?true:false;    // Con el equals también funcionaría.
+
+                Evento evento = new Evento(id, nombre, fecha, ciudad, descripcion, duracion, precio, organizador, gratuito);
+                System.out.println(evento);
+
+                if (mapaEventos.get(ciudad)==null) {
+                    List<Evento> eventosCiudad = new ArrayList<>();
+                    eventosCiudad.add(evento);
+                    mapaEventos.put(ciudad, eventosCiudad);
+                } else {
+                    mapaEventos.get(ciudad).add(evento);
+                }
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        return mapaEventos;
 
     }
 }
